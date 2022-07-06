98b3356aa2dd18e060fb41cade17c4795502658c
Elliot Berry <eberry@breaktimemedia.com>
2022-07-06 00:01:41 -0400

    making a lil bit modern

--- src/lsh.js
+++ src/lsh.js
@@ -1,56 +1,51 @@
-'use strict';
-
 /**
-* Main class for indexing Minhash signatures
-**/
+ * Main class for indexing Minhash signatures
+ **/
 
-var LshIndex = function(args) {
+class LshIndex {
+  constructor(args) {
+    
   var args = args || {};
   this.bandSize = args.bandSize || 4;
   this.index = {};
+  }
 
-  this.insert = function(key, minhash) {
-    var hashbands = this.getHashbands(minhash);
-    for (var i=0; i<hashbands.length; i++) {
-      var band = hashbands[i];
+  insert(key, minhash) { 
+    const hashbands = this.getHashbands(minhash);
+
+    hashbands.forEach(band => {
       if (Array.isArray(this.index[band])) {
         this.index[band].push(key);
       } else {
         this.index[band] = [key];
       }
-    };
+    });
   };
 
-  this.query = function(minhash) {
-    var matches = {};
-    var hashbands = this.getHashbands(minhash);
-    for (var i=0; i<hashbands.length; i++) {
-      var band = hashbands[i];
-      for (var j=0; j<this.index[band].length; j++) {
+ query(minhash) {
+    const matches = {};
+    const hashbands = this.getHashbands(minhash);
+
+    hashbands.forEach(band => {
+      for (let j = 0; j < this.index[band].length; j++) {
         matches[this.index[band][j]] = true;
-      };
-    };
+      }
+    });
+
     return Object.keys(matches);
   };
 
-  this.getHashbands = function(minhash) {
+  getHashbands(minhash) {
     if (minhash.hashbands) return minhash.hashbands;
     minhash.hashbands = [];
-    for (var i=0; i<minhash.hashvalues.length / this.bandSize; i++) {
-      var start = i * this.bandSize;
-      var end = start + this.bandSize;
-      var band = minhash.hashvalues.slice(start, end);
+    for (let i = 0; i < minhash.hashvalues.length / this.bandSize; i++) {
+      const start = i * this.bandSize;
+      const end = start + this.bandSize;
+      const band = minhash.hashvalues.slice(start, end);
       minhash.hashbands.push(band.join('.'));
-    };
+    }
     return minhash.hashbands;
   };
 };
 
-if (typeof window !== 'undefined') window.LshIndex = LshIndex;
-
-if (typeof exports !== 'undefined') {
-  if (typeof module !== 'undefined' && module.exports) {
-    exports = module.exports = LshIndex;
-  }
-  exports = LshIndex;
-}
+export default LshIndex;
